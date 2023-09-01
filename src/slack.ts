e4b740001c5a38f32295612ac2c5447fa2dc69bf
David Jiménez <david.jimenezj@edreamsodigeo.com>
2023-09-01 14:39:50 +0200

    chore: pretty files

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,14 @@
 import type { Handler } from '@netlify/functions';
 
 export const handler: Handler = async () => {
-	// TODO validate the Slack request
+  // TODO validate the Slack request
 
-	// TODO handle slash commands
+  // TODO handle slash commands
 
-	// TODO handle interactivity (e.g. context commands, modals)
+  // TODO handle interactivity (e.g. context commands, modals)
 
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
+  return {
+    statusCode: 200,
+    body: 'TODO: handle Slack commands and interactivity',
+  };
 };
